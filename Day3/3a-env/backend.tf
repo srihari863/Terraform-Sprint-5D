e25faca61dd47a9c7835503c5879e5632c78fa45
srihari863 <116693272+srihari863@users.noreply.github.com>
2024-07-10 08:45:35 +0530

    Delete Day3/3a-env directory

--- Day3/3a-env/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstate2347abc"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
-}
--- Day3/3a-env/output.tf
+++
@@ -1,7 +0,0 @@
-output "resource_group_id" {
-  value = azurerm_resource_group.example.id
-}
-
-output "storage_account_id" {
-  value = azurerm_storage_account.example.id
-}
